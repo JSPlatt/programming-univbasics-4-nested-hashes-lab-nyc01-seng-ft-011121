d0eea1dbda26d7c363079fa9df9d8ba7ef1f124a
Jeffrey Platt <jsplatt@gmail.com>
2020-12-03 17:06:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,9 +85,9 @@ programmer_hash[:yukihiro_matsumoto] = [
   { :known_for=> "Ruby"},
   { :languages=> "LISP", "C"}
   ]
-return programmer_hash
 end
-
+programmer_hash
+end
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
