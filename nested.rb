e97150da143897af0e81c9ba9e3c3473e7e127ae
Jeffrey Platt <jsplatt@gmail.com>
2020-12-03 17:11:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,10 +82,12 @@ def adding_matz
       }
     }
 programmer_hash[:yukihiro_matsumoto]=[]
+    {
       :yukihiro_matsumoto => {
       :known_for=> "Ruby",
       :languages=> ["LISP", "C"]
       }
+    }  
 return programmer_hash
       
 end
